adf86b768c7974a8166a50988059bbc6768d53f2
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2023-01-17 19:00:30 +0700

    hello

--- 4.Access-Specifiers_and_func.cpp
+++ 4.Access-Specifiers_and_func.cpp
@@ -5,13 +5,19 @@ class knight
     public:
         std::string name;
         int attack;
+    void hello()
+    {
+        std::cout << "hello";
+    }
     private:
         int deffend;
+
 
 };
 int main()
 {
     knight john;
     john.name = "john";
-    john.deffend = 50;
+    // john.deffend = 50;
+    john.hello();
 }
--- 5.Inheritance.cpp
+++ 5.Inheritance.cpp
@@ -5,7 +5,7 @@ class knight //parent
     public:
         std::string name;
         int attack;
-    private:
+    protected:
         int deffend;
     
 
@@ -15,13 +15,24 @@ class light :public knight //child
 {
     public:
         int bonus_atk;
+
+        void set_def(int def)
+        {
+            deffend = def;
+        }
+        void display()
+        {
+            std::cout << "protect = " << deffend << std::endl;
+        }
 };
 
 int main()
 {
-    light son;
+    light son;s
     son.attack = 10;
     son.bonus_atk = 50;
     int total = son.attack * son.bonus_atk;
+    son.set_def(50);
+    son.display();
     std::cout <<"son atk = " << son.attack << " bonus = "<< son.bonus_atk << " total = "<< total;
 }
